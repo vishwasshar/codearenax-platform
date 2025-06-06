c7b59e56e1ae9d3e5e760a4d32673eac214e5ffc
Vishwas Sharma <vishwassharma3287@gmail.com>
2025-06-06 11:54:54 +0530

    Created a Full Screen Text Editor Component using Monaco with a language swithcer and code validation.

---
+++ frontend/components/TextEditor.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+import Editor from '@monaco-editor/react';
+import * as monaco from "monaco-editor";
+
+const languages = ["javascript","python","cpp"];
+
+function TextEditor() {
+  const [code, setCode] = useState<string>("");
+  const [language,setLanguage] = useState<string>("javascript");
+
+
+  const handleEditorChange = (value: string | undefined) => {
+    setCode(value || "");
+  };
+
+  const handleCodeValidation = (markers:monaco.editor.IMarker[])=>{
+
+    markers.forEach((marker:monaco.editor.IMarker) => {
+      console.log(marker.message);
+    });
+
+  }
+
+
+  return (
+    <div className='w-full h-screen flex flex-col gap-2'>
+      <div className='flex justify-end '>
+        <select onChange={(e)=>{
+          setLanguage(e.target.value);
+        }} className="select select-ghost w-50">
+          {languages.map((lang)=><option value={lang}>{lang}</option>)}
+        </select>
+      </div>
+       <Editor
+        height="100%"
+        language={language}
+        defaultValue={code}
+        theme="vs-dark"
+        onChange={handleEditorChange}
+        onValidate={handleCodeValidation}
+      />
+    </div>
+  );
+  
+}
+
+
+export default TextEditor;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+import './App.css';
+import TextEditor from "../components/TextEditor";
 
 function App() {
 
   return (
     <>
+      <TextEditor/>
     </>
   )
 }
