00cd7875bf8d73f808b74777cc39e9409ead09b9
Vishwas Sharma <vishwassharma3287@gmail.com>
2025-06-10 11:05:54 +0530

    Added @nestjs/config @nestjs/mongoose mongoose dependencies and connected with mongo.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CodeSyncModule } from './codeSync/codeSync.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, CodeSyncModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}),
+    UsersModule, 
+    CodeSyncModule, 
+
+    MongooseModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory:(configService:ConfigService)=>({
+        uri: configService.get<string>('MONGOOSE_URI',{infer:true})
+      })
+    })
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
