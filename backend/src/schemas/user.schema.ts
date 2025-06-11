c62345fcd2c6fd926e3ba189d1f8dff6fd8039d0
Vishwas Sharma <vishwassharma3287@gmail.com>
2025-06-11 12:25:51 +0530

    Added 1-N relationship between user and room, added role field using enum.

--- backend/src/schemas/user.schema.ts
+++ backend/src/schemas/user.schema.ts
@@ -1,4 +1,8 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+import { Room } from "./room.schema";
+import { UserRoles } from "src/common/enums";
+
 
 @Schema()
 export class User{
@@ -8,9 +12,16 @@ export class User{
 
     @Prop({required:true, unique:true})
     email:string;
+
+    @Prop({required:true, enum:UserRoles, default:UserRoles.USER})
+    role:UserRoles;
     
     @Prop({required:true})
     password:string;
+
+    // 1-Many Relationship with Room Schema
+    @Prop({type:[{type:mongoose.Schema.Types.ObjectId, ref: 'Room'}]})
+    rooms:Room[];
 }
 
 // Creating a Schema using a Class
