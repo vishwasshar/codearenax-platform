08d3528fe47c39fcdfe5cdf79d77d49fd2196f39
Vishwas Sharma <vishwassharma3287@gmail.com>
2025-06-12 13:12:23 +0530

    Added Basic CRUD Operations for Rooms

--- backend/src/rooms/rooms.controller.ts
+++ backend/src/rooms/rooms.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Param, Patch, Post, ValidationPipe } from '@nestjs/common';
+import { RoomsService } from './rooms.service';
+import mongoose from 'mongoose';
+import { CreateRoom } from './dtos/CreateRoom.dto';
+import { UpdateRoom } from './dtos/UpdateRoom.dto';
 
 @Controller('rooms')
-export class RoomsController {}
+export class RoomsController {
+    constructor(private readonly roomsService:RoomsService){}
+
+    @Get()
+    getAllRooms(){
+        return this.roomsService.getAllRooms();
+    }
+
+    @Get(":id")
+    getRoomById(@Param("id") id:string){
+        if(!mongoose.Types.ObjectId.isValid(id)) throw new HttpException("Invalid Id",400);
+        
+        const room = this.roomsService.getRoomById(id);
+        
+        if(!room) throw new HttpException("Room Not Found",404);
+        
+        return room;
+    }
+    
+    @Post()
+    createNewRoom(@Body(new ValidationPipe()) createRoom:CreateRoom){
+        
+        const room = this.roomsService.createNewRoom(createRoom);
+        
+        return room;
+    }
+    
+    @Patch(":id")
+    updateRoom(@Param("id") id:string,@Body(new ValidationPipe()) updateRoom:UpdateRoom){
+        if(!mongoose.Types.ObjectId.isValid(id)) throw new HttpException("Invalid Id",400);
+        
+        const room = this.roomsService.updateRoom(id,updateRoom);
+        
+        if(!room) throw new HttpException("Room Not Found",404);
+        
+        return room;
+    }
+}
--- backend/src/rooms/rooms.service.ts
+++ backend/src/rooms/rooms.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Room } from 'src/schemas/room.schema';
+import { CreateRoom } from './dtos/CreateRoom.dto';
+import { UpdateRoom } from './dtos/UpdateRoom.dto';
 
 @Injectable()
-export class RoomsService {}
+export class RoomsService {
+
+    constructor(@InjectModel(Room.name) private roomModel:Model<Room>){}
+
+    getAllRooms(){
+        return this.roomModel.find();
+    }
+
+    getRoomById(id:string){
+        return this.roomModel.findById(id);
+    }
+
+    createNewRoom(createRoom: CreateRoom){
+        return this.roomModel.create(createRoom);
+    }
+
+    updateRoom(id:string, updateRoom: UpdateRoom){
+        return this.roomModel.findByIdAndUpdate(id, updateRoom, {new:true});
+    }
+}
