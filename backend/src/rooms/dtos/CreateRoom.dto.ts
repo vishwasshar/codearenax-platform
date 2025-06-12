1f9c617189fb668eca67bf60caa259dc3fed5631
Vishwas Sharma <vishwassharma3287@gmail.com>
2025-06-12 13:11:50 +0530

    Added Create and Update DTO for Rooms

---
+++ backend/src/rooms/dtos/CreateRoom.dto.ts
@@ -0,0 +1,14 @@
+import { IsEnum, MinLength } from "class-validator";
+import { LangTypes } from "src/common/enums";
+
+export class CreateRoom {
+
+
+    @MinLength(3, {message:"Please enter a name with atleast 3 Characters."})
+    name: string;
+
+    content: string;
+    
+    @IsEnum(LangTypes,{message:"Currently available languages are: " + Object.values(LangTypes).join(", ")})
+    lang: LangTypes;
+}
---
+++ backend/src/rooms/dtos/UpdateRoom.dto.ts
@@ -0,0 +1,9 @@
+import { IsEnum, MinLength } from "class-validator";
+import { LangTypes } from "src/common/enums";
+
+export class UpdateRoom {
+    content: string;
+    
+    @IsEnum(LangTypes,{message:"Currently available languages are: " + Object.values(LangTypes).join(", ")})
+    lang: LangTypes;
+}
