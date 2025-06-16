45959a550e7fd59745de3e6acb9918057efca032
Vishwas Sharma <vishwassharma3287@gmail.com>
2025-06-16 14:45:28 +0530

    Configured Redux Toolkit and Redux Persist for user login and logout.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,19 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ToastContainer stacked hideProgressBar={true} />
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,20 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
+import userReducer from "./slices/user.slice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({ reducer: persistedReducer });
+
+export const persistor = persistStore(store);
