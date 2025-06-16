fb4dda0b736138ccbe448f63fa2275204ed15ab7
Vishwas Sharma <vishwassharma3287@gmail.com>
2025-06-16 11:14:51 +0530

    Setup Toast Container

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ToastContainer stacked hideProgressBar={true} />
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
