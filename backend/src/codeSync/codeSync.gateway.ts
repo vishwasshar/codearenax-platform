f124bd95956c66590807c2aacba35b5d32566cac
Vishwas Sharma <vishwassharma3287@gmail.com>
2025-06-18 15:41:38 +0530

    Updated Socket based syncronization flow to handle: 1. User Authentication. 2. Permission based edit and view. 3. In Memory and Database Data Storage.

--- backend/src/codeSync/codeSync.gateway.ts
+++ backend/src/codeSync/codeSync.gateway.ts
@@ -1,22 +1,154 @@
-import { OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import {Socket, Server} from "socket.io";
+import { JwtService } from '@nestjs/jwt';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+import { AccessRole } from 'src/common/enums/access-role.enum';
+import { RoomsService } from 'src/rooms/rooms.service';
 
-@WebSocketGateway(3002,{cors:{origin:"*"}})
-export class CodeSyncGateway implements OnGatewayConnection{
+@WebSocketGateway(3002, { cors: { origin: '*' } })
+export class CodeSyncGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(
+    private jwtService: JwtService,
+    private readonly roomsService: RoomsService,
+  ) {}
 
-    private code:string = "console.log('it's working');";
+  @WebSocketServer() server: Server;
 
-    handleConnection(client: any, ...args: any[]) {
-        client.emit("code:updatedCode",this.code);
+  // Store detail of rooms (room._id, room - obj.)
+  private activeRooms: Map<string, any> = new Map();
+
+  // Store detail of client socket id and user id (client.id, user._id)
+  private userIds: Map<string, string> = new Map();
+
+  // Handle user socket connection request
+  async handleConnection(client: any, ...args: any[]) {
+    try {
+      const user = await this.jwtService.verify(client.handshake.auth.token, {
+        secret: process.env.JWT_SECRET,
+      });
+
+      this.userIds.set(client.id, user._id);
+    } catch (err) {
+      client.disconnect();
     }
+  }
 
-    @WebSocketServer() server:Server;
+  // Handle user socket disconnect request
+  handleDisconnect(client: any) {
+    try {
+      const rooms = Array.from(client);
 
-    @SubscribeMessage("code:sync")
-    handleNewMessage(client:Socket,message:string){
+      // Iterate over all users active rooms
+      rooms.forEach(async (roomId: string) => {
+        let roomDetails = this.activeRooms
+          .get(roomId)
+          .activeUsers.filter((user: any) => user != client.id);
 
-        this.code = message;
+        // If room doesn't have any active user then update to DB.
+        if (roomDetails.activeUsers.length == 0) {
+          await this.roomsService.updateRoom(roomId, roomDetails);
+          this.activeRooms.delete(roomId);
+        } else {
+          this.activeRooms.set(roomId, roomDetails);
+        }
+      });
+    } catch (err) {}
+  }
 
-        this.server.emit("code:updatedCode",message);
+  // Handle User Room Join Request
+  @SubscribeMessage('room:join')
+  async handleRoomJoin(client: Socket, roomId: string) {
+    try {
+      let room: any;
+
+      // Check If user avaiable inMemory else fetched from DB and stored to inMemory
+      if (this.activeRooms.has(roomId)) {
+        room = this.activeRooms.get(roomId);
+      } else {
+        room = await this.roomsService.getRoomById(roomId);
+
+        room.activeUsers = [];
+        this.activeRooms.set(roomId, room);
+      }
+
+      // Check if user present in access list of room
+      //   IF exists then join the room and add socket id to active user list of room.
+      //   ELSE Through Error
+      if (
+        room?.accessList?.some(
+          (user: any) => user.user == this.userIds.get(client.id),
+        )
+      ) {
+        let room = this.activeRooms.get(roomId);
+        room.activeUsers.push(client.id);
+
+        this.activeRooms.set(roomId, room);
+
+        client.join(roomId);
+
+        const { content, lang } = this.activeRooms.get(roomId);
+
+        client.emit('room:init', { content, lang });
+      } else {
+        client.emit('room:error', 'Not Accessible');
+      }
+    } catch (err) {
+      console.log(err);
+      client.emit('room:error', 'Failed to Join Room');
     }
+  }
+
+  // Handle User leave any room
+  @SubscribeMessage('room:leave')
+  async handleRoomLeave(client: Socket, roomId: string) {
+    try {
+      let room: any = this.activeRooms.get(roomId);
+
+      // If the room is empty then save it to DB.
+      if (room.activeUsers.length == 0) {
+        await this.roomsService.updateRoom(roomId, room);
+        this.activeRooms.delete(roomId);
+      } else {
+        this.activeRooms.set(roomId, room);
+      }
+
+      client.leave(roomId);
+    } catch (err) {
+      client.emit('room:error', 'Failed to leave Room');
+    }
+  }
+
+  // Handle the edit request on the file.
+  @SubscribeMessage('room:edit')
+  handleNewMessage(client: Socket, message: any) {
+    let room = this.activeRooms.get(message.roomId);
+
+    const userId = this.userIds.get(client.id);
+
+    // Check if user have authorization to edit the file
+    if (
+      room &&
+      room?.accessList?.some(
+        (user: any) =>
+          user.user == userId &&
+          (user.role == AccessRole.OWNER || user.role == AccessRole.EDITOR),
+      )
+    ) {
+      room.content = message.content;
+      room.lang = message.lang;
+
+      // Updating Code in inMemory Storage
+      this.activeRooms.set(message.roomId, room);
+
+      // Sending received data to all of the active socket members
+      client.to(message.roomId).emit('room:update', message);
+    }
+  }
 }
--- backend/src/codeSync/codeSync.module.ts
+++ backend/src/codeSync/codeSync.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CodeSyncGateway } from './codeSync.gateway';
+import { JwtService } from '@nestjs/jwt';
+import { RoomsService } from 'src/rooms/rooms.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Room, RoomSchema } from 'src/schemas/room.schema';
 
-@Module({providers:[CodeSyncGateway]})
-export class CodeSyncModule {
-    
-}
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Room.name, schema: RoomSchema }]),
+  ],
+  providers: [CodeSyncGateway, JwtService, RoomsService],
+})
+export class CodeSyncModule {}
