988688755c25863fb3bcebb8dfe6d95437fc035e
Vishwas Sharma <vishwassharma3287@gmail.com>
2026-01-20 14:06:08 +0530

    fix: set non required fields as optional for Update Room DTO

--- backend/src/rooms/dtos/UpdateRoom.dto.ts
+++ backend/src/rooms/dtos/UpdateRoom.dto.ts
@@ -1,9 +1,13 @@
-import { IsEnum, MinLength } from "class-validator";
-import { LangTypes } from "src/common/enums";
+import { IsEnum } from 'class-validator';
+import { LangTypes } from 'src/common/enums';
 
 export class UpdateRoom {
-    content: string;
-    
-    @IsEnum(LangTypes,{message:"Currently available languages are: " + Object.values(LangTypes).join(", ")})
-    lang: LangTypes;
+  content?: string;
+
+  @IsEnum(LangTypes, {
+    message:
+      'Currently available languages are: ' +
+      Object.values(LangTypes).join(', '),
+  })
+  lang?: LangTypes;
 }
