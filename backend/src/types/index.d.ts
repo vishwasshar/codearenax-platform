77ddb844464967dd6c877559f2aeee61bca2e38d
Vishwas Sharma <vishwassharma3287@gmail.com>
2026-01-28 15:24:05 +0530

    feat: added user search endpoint and admin authorization on user routes

--- backend/src/types/index.d.ts
+++ backend/src/types/index.d.ts
@@ -1,11 +1,20 @@
+import { Types } from 'mongoose';
+import { UserRoles } from 'src/common/enums';
 import { AccessRole } from 'src/common/enums/access-role.enum';
 import { User } from 'src/schemas/user.schema';
+
+export {};
 
 declare global {
   namespace Express {
     interface Request {
-      roomRole?: AccessRole;
-      user?: User;
+      roomRole?: AwccessRole;
+    }
+    interface User {
+      _id?: string;
+      name?: string;
+      email?: string;
+      role?: UserRoles;
     }
   }
 }
